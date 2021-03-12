04bb088d16eab8baf22a371cfedcdaabf41a982e
slavvc <slavvc@gmail.com>
2021-03-12 23:17:37 +0300

    chore: use database_url config variable

--- db_definitions.py
+++ db_definitions.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
+import os
+
 engine = create_engine(
-    'sqlite:///db.sqlite',
+    os.environ['DATABASE_URL'],
     connect_args={'check_same_thread': False}
 )
 DBSession = sessionmaker(bind=engine)
